9b5fe6d9966c490a22651612f6c58a40025e71af
Qasimm-Ali <qqali776655@gmail.com>
2024-07-13 15:33:06 +0500

    Added todos in the lists

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -18,11 +18,11 @@ export default function Todo() {
         <input type="text" name="input" ref={ref} className="border-[1px] border-black mr-3 focus:border-[2px] rounded-md focus:border-black outline-none p-2 " />
         <button onClick={handleAdd} className="bg-purple-600 text-white w-[3.5rem] h-[2.1rem] font-medium   rounded-md  hover:bg-purple-500 ">Add</button>
       </div>
-      <div>
+        <div className="flex flex-wrap gap-3 my-3 ">
         {Todo.map((item, ind) => (
-          <li key={ind}>{item} </li>
-        ))}
-      </div>
+            <div className="relative w-[20rem] h-[10rem] border-[2.5px]  border-purple-600 rounded-md overflow-hidden overflow-y-auto  ">{item} <button className="bg-purple-400 text-white w-[3.5rem] h-[2.1rem] font-medium   rounded-md  hover:bg-purple-500 absolute bottom-1 right-2 ">delete</button></div>
+          ))}
+          </div>
     </>
   );
 }
